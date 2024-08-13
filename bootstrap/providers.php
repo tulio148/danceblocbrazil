6be8a08dcc9b473e946c84738d2be48750bbd5c5
Tulio Gerbassi <tuliogerbassi@gmail.com>
2024-08-13 16:35:10 +0800

    providers bug fix

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,7 +5,6 @@ return [
     App\Providers\AuthServiceProvider::class,
     App\Providers\SquareServiceProvider::class,
     App\Providers\EventServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
     App\Providers\StudentServiceProvider::class,
     App\Providers\ClassesServiceProvider::class,
     App\Providers\OrdersServiceProvider::class,
