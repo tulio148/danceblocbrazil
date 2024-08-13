d12ba645b65ded5b8625a13d9f83685afd78aabd
Tulio Gerbassi <tuliogerbassi@gmail.com>
2024-08-13 16:25:11 +0800

    providers bug

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,14 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\SquareServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    App\Providers\StudentServiceProvider::class,
+    App\Providers\ClassesServiceProvider::class,
+    App\Providers\OrdersServiceProvider::class,
+    App\Providers\PaymentServiceProvider::class,
+    App\Providers\CardsServiceProvider::class,
+    \SocialiteProviders\Manager\ServiceProvider::class
 ];
