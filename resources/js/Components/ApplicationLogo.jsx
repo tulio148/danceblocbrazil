b8f843508b7d1119c243f9a942f44e5b6fba19d6
Tulio Gerbassi <tuliogerbassi@gmail.com>
2024-08-18 16:15:10 +0800

    logo size fix

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,5 +1,9 @@
 export default function ApplicationLogo({ classNames }) {
     return (
-        <img className={`${classNames} w-16 `} src="/logonobg.webp" alt="" />
+        <img
+            className={`${classNames} w-16 h-auto`}
+            src="/logonobgsm.webp"
+            alt=""
+        />
     );
 }
