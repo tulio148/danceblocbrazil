ab24cdbb85bc08d7c0ff2c5d8214d6c28bf9d2a1
Ubuntu <ubuntu@ip-172-26-14-249.ap-southeast-2.compute.internal>
2024-08-20 08:53:01 +0000

    new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,30 @@ import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./resources/js/**/*.jsx",
-    ],
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.jsx",
+  ],
 
-    theme: {
-        extend: {
-            colors: {
-                "db-pink": "#990093",
-                "db-light-pink": "#FF00F7",
-                "db-green": "#00FFA0",
-                "special-blue": "#5c749d",
-            },
-            fontFamily: {
-                // sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-                sans: ["Raleway", ...defaultTheme.fontFamily.sans],
-            },
-            transitionDuration: {
-                2000: "2000ms",
-            },
-        },
+  theme: {
+    extend: {
+      colors: {
+        "db-pink": "#EB00DE",
+        "db-light-pink": "#FF00F7",
+        "db-green": "#00FFA0",
+        "special-blue": "#5c749d",
+      },
+      fontFamily: {
+        // sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+        sans: ["Raleway", ...defaultTheme.fontFamily.sans],
+      },
+      transitionDuration: {
+        2000: "2000ms",
+      },
     },
+  },
 
-    plugins: [forms],
+  plugins: [forms],
 };
